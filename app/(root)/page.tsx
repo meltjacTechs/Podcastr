088a8516c8d314d8c73cfda28f58d9d8d7fdad79
meltjacTechs <ebwire02@gmail.com>
2024-06-07 01:00:06 +0300

    Creating blueprint layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="mt-9 flex-col gap-9">
+      <section className="flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">Trending Podcast</h1>
+        <Button className="text-white-1 bg-orange-1">Button</Button>
+      </section>
+    </div>
+  );
+}
