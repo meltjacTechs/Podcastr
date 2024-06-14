1c2bd91f33c305fdce50d99e498433bcf3c9ae5a
meltjacTechs <ebwire02@gmail.com>
2024-06-14 19:09:07 +0300

    auth_update and convex utilities implemented

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+const authConfig = {
+    providers:[
+        {
+           domain:  "https://aware-quagga-86.clerk.accounts.dev ",
+           applicationID: "convex",
+        },
+    ]
+};
+export default authConfig;
