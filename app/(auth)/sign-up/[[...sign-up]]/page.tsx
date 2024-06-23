0653ce664a8b10fbda6e094abac7d824d1fb3cae
meltjacTechs <ebwire02@gmail.com>
2024-06-23 13:46:07 +0300

    feat: Remove unused profile and podcast details pages

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,9 @@
 import { SignUp } from '@clerk/nextjs'
-import React from 'react'
 
 const Page = () => {
   return (
     <div className="flex-center glassmorphism-auth h-screen w-full">
-        <SignUp />
+      <SignUp />
     </div>
   )
 }
